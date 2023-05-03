dc5fe872d6279f80388dcc37b73504c73d8b7ecb
Dev Su Su <suphonemyint.la@gmail.com>
2023-05-04 01:19:28 +0630

    forkify app recipe

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2529c363-03bd-4e1a-86db-78e365156a9a';
+export const MODAL_CLOSE_SEC = 2.5;
